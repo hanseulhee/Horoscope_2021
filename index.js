8f60d35ff09f00f210fa68dfdbc557d90c9e9015
hanseulhee <3021062@gmail.com>
2021-02-03 00:51:08 +0900

    web
    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+const back = () => {
+  let url = window.location.href;
+  let new_url = url.split("result.html")[0] + "index.html";
+  window.location.href = new_url;
+};
+
+const share = () => {
+  const t = document.createElement("textarea");
+  document.body.appendChild(t);
+  t.value = window.location.href;
+  t.select();
+  document.execCommand("copy");
+  document.body.removeChild(t);
+  alert("복사 완료!");
+};
